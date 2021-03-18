d16ebe5874829511f6b9177374ae1cbc6528203b
Christian Mena <christian.91.gm@hotmail.com>
2021-03-18 18:33:50 -0500

    Cheking contract balance

--- MessageStore.sol
+++ MessageStore.sol
@@ -23,5 +23,13 @@ contract MessageStore is Ownable{
         return message;
     }
     
+    function getBalance() public view returns (uint){
+        return address(this).balance;
+    }
+    
+    
+    function getBalanceInEther() public view returns(uint){
+        return getBalance() /1e18;
+    }
     
 }
